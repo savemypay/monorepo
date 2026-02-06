b66322fc5deff78a162a883327422dc9c91a8d37
girishbabucfg <girish.babu@cafgia.com>
2026-02-06 12:52:08 +0530

    git ignore update

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.add_middleware(RequestLoggingMiddleware)
 app.add_middleware(SecurityHeadersMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
